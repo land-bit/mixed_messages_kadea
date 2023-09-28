6f19c0fadaf18a234b72a1e36586ff3d12da215b
landrybit <landrybitege7@gmail.com>
2023-09-28 11:22:51 +0100

    Finish a function

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 //Creation de 3 tableaux qui constituera la phrase
 
-let array1 = ['Landry', 'Aubrey', 'Chris', 'Alfred', 'deo', 'cyrus', 'dadou', 'felly', 'Papy', 'Gaby'];
+let array1 = ['Landry', 'Aubrey', 'Chris', 'Alfred', 'Deo', 'Cyrus', 'Dadou', 'Felly', 'Papy', 'Gaby'];
 let array2 = ['code','est', 'mange', 'va', 'connait', 'danse', 'est', 'coupe', 'gaspille', 'veut'];
-let array3 = [ 'surement', 'inteligent', 'trop vite', 'couragieux', 'baiser', 'souvent', 'en reunion' ,'doué', 'incapable', 'capable']
+let array3 = [ 'surement', 'inteligent', 'trop vite', 'courageux', 'baiser', 'souvent', 'en reunion' ,'doué', 'incapable', 'capable']
 
 //creation de la fonction qui affichera la phrase
+
+let a = array1[Math.floor(Math.random()*9)]
+let b = array2[Math.floor(Math.random()*9)]
+let c = array3[Math.floor(Math.random()*9)]
+
+console.log(a, b, c)
