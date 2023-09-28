86455921b13b8f767bbcb60a1a3c1a49af36a40f
User <mukalally155@gmail.com>
2023-09-28 13:07:51 +0200

    sens of sentences

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //Creation de 3 tableaux qui constituera la phrase
 
 let array1 = ['Landry', 'Aubrey', 'Chris', 'Alfred', 'Deo', 'Cyrus', 'Dadou', 'Felly', 'Papy', 'Gaby'];
-let array2 = ['code','est', 'mange', 'va', 'connait', 'danse', 'est', 'coupe', 'gaspille', 'veut'];
-let array3 = [ 'surement', 'inteligent', 'trop vite', 'courageux', 'baiser', 'souvent', 'en reunion' ,'doué', 'incapable', 'capable']
+let array2 = ['est', 'devient', 'reste', 'semble', 'se fait', 's\'avere', 'parrait', 'demeure', 'a l\'air', 's\'affirme'];
+let array3 = [ 'proffessionel', 'inteligent', 'atténtif', 'courageux', 'fatigué', 'consideré', 'determiné' ,'admiré', 'incapable', 'capable'];
 
 //creation de la fonction qui affichera la phrase
 
